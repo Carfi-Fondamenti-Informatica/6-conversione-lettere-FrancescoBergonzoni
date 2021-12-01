886dc96f8f8a1ed7deae4255335cbb4b347dec29
FrancescoBergonzoni <91878700+FrancescoBergonzoni@users.noreply.github.com>
2021-12-01 16:59:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+char a;
+int main() {
+
+    //table();
+    //do{
+        cin >> a;
+        if (verifica(a)){
+            if (int(a) >= 97 && int(a) <= 122) {
+                a = char(int(a) - 32);
+            } else {
+                a = char(int(a) + 32);
+            }
+            cout << a<< endl;
+        }else{
+            cout<<"errore"<<endl;
+        }
+    //}while(a!=0);
+        return 0;
+
 }
